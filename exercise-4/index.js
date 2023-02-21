eabb44d951a5cbfc8eee79a59f7c5cc475672fe9
jericokatong <jericokatong@gmail.com>
2023-02-21 10:12:45 +0800

    ubah exercise-3 dengan menggunakan express

--- exercise-4/index.js
+++ exercise-4/index.js
@@ -34,13 +34,32 @@
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
+const dataMember = require("./members.js");
+const dataUsers = require("./users.js");
+const moment = require("moment");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  // res.setHeader("content-type", "text/plain");
-  res.write("This is the home page");
+  res.setHeader("content-type", "text/plain");
+  res.send("This is the home page");
+});
+
+app.get("/about", (req, res) => {
+  res.setHeader("content-type", "text/json");
+  res.json({
+    status: "success",
+    message: "response success",
+    description: "Exercise #03",
+    date: moment().format(),
+    data: dataMember,
+  });
+});
+
+app.get("/users", (req, res) => {
+  res.setHeader("content-type", "text/json");
+  res.json(dataUsers);
 });
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
